69e8d165fd21a76236e663a40affe846ce575255
Alexander Wennerstrøm <alexanderw0310@gmail.com>
2021-10-17 00:03:20 +0200

    Moved Custom Exceptions into index file

--- src/Drivers/index.ts
+++ src/Drivers/index.ts
@@ -8,13 +8,8 @@ import {
   CannotDeleteFileException,
   CannotGetMetaDataException,
   CannotSetVisibilityException,
+  Exception,
 } from '@adonisjs/core/build/standalone'
-
-import {
-  CannotCreateContainerException,
-  CannotDeleteContainerException,
-  CannotFindContainerException,
-} from './utils'
 
 import {
   Visibility,
@@ -43,6 +38,33 @@ import {
   ContainerDeleteResponse,
 } from '@azure/storage-blob'
 import { ReadStream } from 'fs'
+
+/**
+ * Unable to write file to the destination
+ */
+export declare class CannotCreateContainerException extends Exception {
+  private containerName: string // A container name
+  private original: any // Original error
+  public static invoke(containerName: string, original: any): CannotCreateContainerException
+}
+
+/**
+ * Unable to get file metadata
+ */
+export declare class CannotFindContainerException extends Exception {
+  private containerName: string // Container name
+  private original: any // Original error
+  public static invoke(containerName: string, original: any): CannotFindContainerException
+}
+
+/**
+ * Unable to delete file from a given location
+ */
+export declare class CannotDeleteContainerException extends Exception {
+  private containerName: string
+  private original: any
+  public static invoke(containerName: string, original: any): CannotDeleteContainerException
+}
 
 export class AzureStorageDriver implements AzureStorageDriverContract {
   /**
